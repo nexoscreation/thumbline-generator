22997fc7500283f50f20314a31fc24a3a1028960
nexoscreator <nexoscreator@gmail.com>
2024-10-07 18:09:40 +0530

    update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,20 +1,27 @@
 <template>
     <div :title="post.title" :description="post.description" :thumb_url="post.thumb_url">
         <ContentDoc />
-        </div>
+    </div>
 </template>
 
-<script lang="ts">
-
-
+<script setup lang="ts">
+    import { defineEventHandler } from 'h3';
     import { defineComponent, ref, onMounted } from 'vue';
+
+    export default defineEventHandler(async (event) => {
+        const posts = await getAllPosts();
+        return posts.map(post => ({ params: { slug: post.slug } }));
+    });
+
+    definePageMeta({
+        layout: 'Post'
+    })
 
     export default defineComponent({
         setup() {
             const post = ref({
                 title: '',
                 description: '',
-                content: '',
                 thumb_url: ''
             });
 
@@ -29,20 +36,6 @@
                 post,
             };
         }
-    });
-</script>
-
-<script setup lang="ts">
-definePageMeta({
-    layout: 'Post'
-})
-
-    // Use this section for fetching the static paths for the dynamic route
-    import { defineEventHandler } from 'h3';
-
-    export default defineEventHandler(async (event) => {
-        const posts = await getAllPosts();
-        return posts.map(post => ({ params: { slug: post.slug } }));
     });
 </script>
 
