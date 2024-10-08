fbc276eba4006819ef3206ef970b53f4be56d46c
nexoscreator <nexoscreator@gmail.com>
2024-10-08 12:05:20 +0530

    Up: ui major changes

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,7 +3,14 @@
         <h1>{{ post.title }}</h1>
         <img :src="ogImageUrl" alt="OG Image" />
         <!-- Rest of your blog post content -->
-        <ContentDoc />
+        <ContentDoc>
+            <template #not-found>
+                <h1>This is Example of Generated og image</h1>
+            </template>
+            <template #empty>
+                <h1>Page is empty</h1>
+            </template>
+        </ContentDoc>
     </div>
 </template>
 <script lang="ts" setup>
